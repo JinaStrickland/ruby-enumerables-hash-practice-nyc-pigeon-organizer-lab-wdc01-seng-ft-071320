88b90bc71028e8e68f126eef78006a0db857da19
Jina Strickland <jina30@gmail.com>
2020-07-04 18:49:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,37 +4,3 @@ def nyc_pigeon_organizer(data)
 
 binding.pry
 end
-
-
-
-# data = {
-#   :color => {
-#     :purple => ["Theo", "Peter Jr.", "Lucky"],
-#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
-#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-#     :brown => ["Queenie", "Alex"]
-#   },
-#   :gender => {
-#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-#     :female => ["Queenie", "Ms. K"]
-#   },
-#   :lives => {
-#     "Subway" => ["Theo", "Queenie"],
-#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
-#     "Library" => ["Peter Jr."],
-#     "City Hall" => ["Andrew"]
-#   }
-# }
-#
-# OUTPUT:
-# pigeon_list = {
-#   "Theo" => {
-#     :color => ["purple", "grey"],
-#     :gender => ["male"],
-#     :lives => ["Subway"]
-#   },
-#   "Peter Jr." => {
-#     :color => ["purple", "grey"],
-#     :gender => ["male"],
-#     :lives => ["Library"]
-#   },
